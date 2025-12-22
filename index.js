f7e52806f4bd232670afcd075d2341d5adde1923
mus github <malpteki0@gmail.com>
2025-12-22 22:49:38 +0100

    wijzig blad half gereed en .env bestand opgezet

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import bodyParser from 'body-parser';
 import pg from "pg";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const db = new pg.Client({
     user: "postgres",
@@ -14,6 +18,24 @@ const db = new pg.Client({
 
 db.connect();
 
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: true,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    }
+});
+
+// transporter.verify(function (error, success) {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log("Server is ready to take messages");
+//     }
+// });
+
 const app = express();
 const port = 3000;
 
@@ -22,8 +44,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 // app.use(express.static("public"));
 
 app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
-// ==>> VOOR DEBUGGING VERPLICHT!!!
+app.set("views", __dirname + "/views");// ==>> VOOR DEBUGGING VERPLICHT!!!
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -61,6 +82,74 @@ app.get("/", async (req, res) => {
     }
 });
 
+app.get("/Wijzig", async (req, res) => {
+    const result = await db.query("SELECT * FROM fiets");
+    const fietsen = result.rows;
+    res.render("Wijzig.ejs", { fietsen: [] })
+});
+
+
+app.post("/Wijzig", async (req, res) => {
+    const { zoektype, zoekwaarde } = req.body;
+
+    // basisvalidatie
+    if (!zoektype || !zoekwaarde) {
+        return res.status(400).send("Onvolledige invoer");
+    }
+
+    try {
+        //     let rows =[];
+
+        //     if (zoektype === "fietsId") {
+        //         // zoeken op fiets ID
+        //         [rows] = await db.query(
+        //             "SELECT * FROM fiets WHERE fietsid = $1",
+        //             [zoekwaarde]
+        //         );
+
+        //     } else if (zoektype === "gebruiker") {
+        //         // zoeken op gebruikersnaam
+        //         const [rows] = await db.query(
+        //             "SELECT * FROM fiets WHERE gebruiker = $1",
+        //             [zoekwaarde]
+        //         );
+        //     }
+        //     res.render("Wijzig.ejs", { fietsen: rows });
+        //     console.log({ fietsen: rows });
+
+        // } catch (err) {
+        //     console.error(err);
+        //     res.status(500).send("Database fout");
+        // }
+
+        let query = "";
+        let values = [zoekwaarde];
+
+        if (zoektype === "fietsId") {
+            query = "SELECT * FROM fiets WHERE fietsid = $1";
+        } else if (zoektype === "gebruiker") {
+            query = "SELECT * FROM fiets WHERE gebruiker = $1";
+        }
+
+        const result = await db.query(query, values);
+
+        if (result.rows.length > 0) {
+            // Er zijn matchende rijen
+            res.render("Wijzig.ejs", { fietsen: result.rows });
+        } else {
+            // Geen match gevonden
+            res.render("Wijzig.ejs", {
+                fietsen: []
+            });
+        }
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send("Database fout");
+    }
+
+});
+
 
 app.post("/", async (req, res) => {
     const { fietsID, naam, gebruiker, datumGekeurd, keuringVervalDatum } = req.body;
@@ -80,8 +169,24 @@ app.post("/", async (req, res) => {
                 `);
     }
     console.log(req.body); // debug
+    const mailOptions = {
+        from: process.env.SMTP_USER,
+        to: process.env.SMTP_USER,
+        subject: "dit is een test met nodemailer",
+        text: "fietsen opstellen voor keuring"
+    };
 
-    // hier komt DB insert
+
+    try {
+        const info = await transporter.sendMail(mailOptions);
+        console.log("Email verzonden:", info.response);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Kon de email niet verzenden.");
+    }
+
+
+
 });
 
 
