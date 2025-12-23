e77938e7b4f064f82a70e9a815721c544a1922ae
mus github <malpteki0@gmail.com>
2025-12-23 02:06:44 +0100

    na afronding op papier, keuringvervaldatum wordt zelf ingevuld door script op index.ejs == datumgekeurd + 1jaar

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "world",
-    password: "12345678",
-    port: 5432,
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT
 });
 
 db.connect();
@@ -59,6 +59,8 @@ function formatDatum(d) {
     return `${dag} / ${maand} / ${jaar}`;
 }
 
+
+
 app.get("/", async (req, res) => {
     try {
         // data ophalen
@@ -74,7 +76,7 @@ app.get("/", async (req, res) => {
         }
         // doorgeven aan EJS
         res.render("index.ejs", { fietsen });
-        console.log(fietsen);
+        // console.log(fietsen);
 
     } catch (err) {
         console.error("database niet bereikbaar:", err.message);
@@ -98,29 +100,6 @@ app.post("/Wijzig", async (req, res) => {
     }
 
     try {
-        //     let rows =[];
-
-        //     if (zoektype === "fietsId") {
-        //         // zoeken op fiets ID
-        //         [rows] = await db.query(
-        //             "SELECT * FROM fiets WHERE fietsid = $1",
-        //             [zoekwaarde]
-        //         );
-
-        //     } else if (zoektype === "gebruiker") {
-        //         // zoeken op gebruikersnaam
-        //         const [rows] = await db.query(
-        //             "SELECT * FROM fiets WHERE gebruiker = $1",
-        //             [zoekwaarde]
-        //         );
-        //     }
-        //     res.render("Wijzig.ejs", { fietsen: rows });
-        //     console.log({ fietsen: rows });
-
-        // } catch (err) {
-        //     console.error(err);
-        //     res.status(500).send("Database fout");
-        // }
 
         let query = "";
         let values = [zoekwaarde];
@@ -129,21 +108,26 @@ app.post("/Wijzig", async (req, res) => {
             query = "SELECT * FROM fiets WHERE fietsid = $1";
         } else if (zoektype === "gebruiker") {
             query = "SELECT * FROM fiets WHERE gebruiker = $1";
+        } else {
+            // Geen match gevonden
+            res.render("Wijzig.ejs", { fietsen: [] });
         }
 
         const result = await db.query(query, values);
 
-        if (result.rows.length > 0) {
-            // Er zijn matchende rijen
-            res.render("Wijzig.ejs", { fietsen: result.rows });
-        } else {
-            // Geen match gevonden
-            res.render("Wijzig.ejs", {
-                fietsen: []
-            });
+        // datums formatteren
+        const fietsen = result.rows;
+
+        for (let i = 0; i < fietsen.length; i++) {
+            fietsen[i].datumgekeurd = formatDatum(fietsen[i].datumgekeurd);
+            // console.log(`${fietsen[i].datumgekeurd}`);
+            fietsen[i].keuringvervaldatum = formatDatum(fietsen[i].keuringvervaldatum);
+            // console.log(`${fietsen[i].keuringvervaldatum}`);
         }
-    }
-    catch (err) {
+
+        res.render("Wijzig.ejs", { fietsen });
+
+    } catch (err) {
         console.error(err);
         res.status(500).send("Database fout");
     }
@@ -168,22 +152,22 @@ app.post("/", async (req, res) => {
         <a href="/">Terug</a>;
                 `);
     }
-    console.log(req.body); // debug
-    const mailOptions = {
-        from: process.env.SMTP_USER,
-        to: process.env.SMTP_USER,
-        subject: "dit is een test met nodemailer",
-        text: "fietsen opstellen voor keuring"
-    };
+    //     console.log(req.body); // debug
+    //     const mailOptions = {
+    //         from: process.env.SMTP_USER,
+    //         to: process.env.SMTP_USER,
+    //         subject: "dit is een test met nodemailer",
+    //         text: "fietsen opstellen voor keuring"
+    //     };
 
 
-    try {
-        const info = await transporter.sendMail(mailOptions);
-        console.log("Email verzonden:", info.response);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Kon de email niet verzenden.");
-    }
+    //     try {
+    //         const info = await transporter.sendMail(mailOptions);
+    //         console.log("Email verzonden:", info.response);
+    //     } catch (error) {
+    //         console.error(error);
+    //         res.status(500).send("Kon de email niet verzenden.");
+    //     }
 
 
 
